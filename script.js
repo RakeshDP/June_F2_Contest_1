13383ddb540ef6b7ac9b4869fd585b2d9170be45
RakeshDP <rakeshpawar1895.rp@gmail.com>
2023-06-17 22:20:43 +0530

    first commit

--- script.js
+++ script.js
@@ -8,20 +8,42 @@ let arr = [
 
 function PrintDeveloperbyMap() {
   //Write your code here , just console.log
+  arr.map((employee)=>{
+    if (employee["profession"] === "developer"){
+      console.log(employee)
+    }
+  } )
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here , just console.log
+  arr.forEach((employee)=>{
+    if (employee["profession"] === "developer"){
+      console.log(employee)
+    }
+  } )
 }
 
 function addData() {
   //Write your code here, just console.log
+  const newEmployee={id:4,name:"susan",age:"20",profession:"intern"}
+  arr.push(newEmployee);
+  console.log(newEmployee);
 }
 
 function removeAdmin() {
   //Write your code here, just console.log
+  arr = arr.filter((employee) => employee.profession !== "admin");
+  console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  const newarr=[
+    {id: 5, name:"Rasika", age:"26", profession:"intern"},
+    {id: 6, name:"Neha", age:"25", profession:"Manager"},
+    {id: 7, name:"Kunal", age:"28", profession:"developer"}
+  ];
+  const concatenatedArray = arr.concat(newarr);
+  console.log(concatenatedArray);
 }
